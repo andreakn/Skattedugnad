9a2d52c26c21b4be31d0b14ccc3cf6fa450b6fd5
Andreas Knudsen <andreas.knudsen@gmail.com>
2014-10-19 14:27:26 +0100

    remove test data

---
+++ InfoDugnad2/Migrations/Steps/M_008_Cleanup_8.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MigSharp;
+
+namespace InfoDugnad2.Migrations.Steps
+{
+	[MigrationExport(Tag="Cleanup")]
+	public class M_008_Cleanup_8:IReversibleMigration
+	{
+		public void Up(IDatabase db)
+		{
+			db.Execute("delete from Request");
+			db.Execute("delete from Person");
+		}
+
+		public void Down(IDatabase db)
+		{
+		}
+	}
+}
